5303be1484e9996bd670be82d159f3cee0a4c77e
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-06-26 23:24:27 +0300

    added elements to list
    added elemets to list and then separeted by slash

---
+++ PushElementToList.js
@@ -0,0 +1,9 @@
+/**
+ * Created by inktech.n.kikov on 26-Jun-16.
+ */
+function push() {
+   let list = [];
+    list.push("pesho","gosho","stamat","unifrii");
+    console.log(list.join("-"));
+}
+push();
